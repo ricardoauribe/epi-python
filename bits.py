976c741dbb7dce42731801fa67b28a8dc6f713da
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-03-03 20:21:26 -0600

    primitive multiply

--- bits.py
+++ bits.py
@@ -64,3 +64,20 @@ def closest_int_same_bit_count(x: int) -> int:
 
 ans = closest_int_same_bit_count(7)
 print(ans)
+
+# Multiply
+print("Multiply")
+
+def multiply(x: int, y:int) -> int:
+	def add(a,b):
+		return a if b == 0 else add(a ^ b, (a & b) << 1)
+
+	running_sum = 0
+	while x:
+		if x & 1:
+			running_sum = add(running_sum, y)
+		x, y = x >> 1, y << 1
+	return  running_sum
+
+ans2 = multiply(3,9)
+print(ans2)
