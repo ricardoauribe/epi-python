d83e4c52723aa9ac87b3789bfd1e1edc4496fdec
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-03-14 09:27:14 -0600

    First approach

--- strings/string_integer_conversion.py
+++ strings/string_integer_conversion.py
@@ -1,2 +1,18 @@
 # String Integer Conversion pp71
 # Time complexity O(n), Space Complexity O(n)
+def string_int_conversion(a: str) -> int:
+	power = 1
+	sign = 1
+	answer = 0
+	for i in range(len(a)):
+		if a[i] == '-' & i == 0:
+			sign = -1
+		elif a[i] == '+' & i == 0:
+			sign = 1
+		else:
+			answer += int(a[~i]) * power
+			power *= 10
+	return answer * sign
+
+b = string_int_conversion("-123")
+print(b)
