eee6a6f5592fe2052b1094fac630e6471c6188c4
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-03-13 20:01:42 -0600

    Added expanded function

--- strings/is_palindromic.py
+++ strings/is_palindromic.py
@@ -2,7 +2,12 @@
 #Time complexity O(n) -> Traverse everything once, Space Complexity O(1) No additional space needed
 
 def is_palindromic(s: str) -> bool:
-	return all(s[i] == s[~i] for i in range(len(s)//2))
+	#The all() function returns True if all items in an iterable are true, otherwise it returns False.
+	#return all(s[i] == s[~i] for i in range(len(s)//2))
+	for i in range(len(s)//2):
+		if s[i] != s[len(s)-i-1]:
+			return False
+	return True
 
 
 print(is_palindromic("aabaa"))
