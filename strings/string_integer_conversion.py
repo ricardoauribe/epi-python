808734656279624efab81845a81f4bfc91738ee9
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-03-14 09:40:45 -0600

    Working not quite the function

--- strings/string_integer_conversion.py
+++ strings/string_integer_conversion.py
@@ -4,13 +4,13 @@ def string_int_conversion(a: str) -> int:
 	power = 1
 	sign = 1
 	answer = 0
-	for i in range(len(a)):
-		if a[i] == '-' & i == 0:
+	for i in range(len(a)-1, -1, -1):
+		if a[i] == '-' and i == 0:
 			sign = -1
-		elif a[i] == '+' & i == 0:
+		elif a[i] == '+' and i == 0:
 			sign = 1
 		else:
-			answer += int(a[~i]) * power
+			answer += int(a[i]) * power
 			power *= 10
 	return answer * sign
 
