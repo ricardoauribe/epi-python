d80f160de13638521424612682a4084a14b3b059
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-03-16 23:04:24 -0600

    Initial Solution

--- strings/convert_base.py
+++ strings/convert_base.py
@@ -1,7 +1,18 @@
 # Convert base pp73
 # Time Complexity O(n), Space Complexity O(n)
+import functools
+import string
 
 def convert_base(num_as_string: str, b1: int, b2: int) ->str:
-	print("hey")
+	def construct_from_base(num_as_int, base):
+		return ('' if num_as_int == 0 else 
+						construct_from_base(num_as_int // base, base)+
+						string.hexdigits[num_as_int % base].upper())
+	
+	is_negative = num_as_string[0] == '-'
+	num_as_int = functools.reduce( 
+		lambda x, c: x *b1 + string.hexdigits.index(c.lower()),
+		num_as_string[is_negative:], 0)
+	return ('-' if is_negative else '') + ('0' if num_as_int == 0 else construct_from_base(num_as_int, b2))
 
-convert_base("123", 1, 2)
+print(convert_base("615", 7, 13))
