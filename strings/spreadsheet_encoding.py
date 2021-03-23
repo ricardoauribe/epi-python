f429a92d2ee3071342642b273d5a2b6cc2940322
Ricardo <ricardoauribe@hotmail.com>
2021-03-22 22:43:08 -0600

    Update spreadsheet_encoding.py

--- strings/spreadsheet_encoding.py
+++ strings/spreadsheet_encoding.py
@@ -2,9 +2,11 @@
 # Time complexity O(n), Space Complexity O(1)
 import functools
 
+# reduce takes every char and multiplies the total per 26 indicating the position in order of magnitude
+# then it sum the actual value of the lettter by taking the ord(number) - ord(A) + 1 to set A = 1
 def ss_decode_col_id(col: str) -> int:
     return functools.reduce(
-        lambda result, c: result * 26 + ord(c) -ord('A') + 1, col, 0
+        lambda result, current: result * 26 + ord(current) -ord('A') + 1, col, 0
     )
 
-print(ss_decode_col_id("AA"))
+print(ss_decode_col_id("ZA"))
