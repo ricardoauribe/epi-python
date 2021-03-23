980807831e8a04d81b6967353eba2103eec94cb9
Ricardo <ricardoauribe@hotmail.com>
2021-03-22 22:47:33 -0600

    Create replace_and_remove.py

---
+++ strings/replace_and_remove.py
@@ -0,0 +1,9 @@
+# Replace and Remove pp74
+# Time Complexity O(n), Space Complexity O(n)
+from typing import List
+
+def replace_and_remove(size:int, s: List[str]) -> int:
+    print("hey")
+
+my_list = ['a', 'a', 'b', 'c']
+replace_and_remove(1, my_list)
