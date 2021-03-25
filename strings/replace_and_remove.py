7c425064ba959a9ddf84ea4b343eca62eef9e208
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-03-24 23:39:18 -0600

    function scaffold still need to review

--- strings/replace_and_remove.py
+++ strings/replace_and_remove.py
@@ -3,7 +3,32 @@
 from typing import List
 
 def replace_and_remove(size:int, s: List[str]) -> int:
-    print("hey")
+    # Forward iteration: remove 'b's and count number of 'a's
+    write_idx = 0
+    a_count = 0
+
+    for i in range(size):
+        if s[i]!= 'b':
+            s[write_idx] = s[i]
+            write_idx += 1
+        if s[i] == 'a':
+            a_count += 1
+
+    # Backwar iteration: replace 'a's with 'dd's starting from the end
+    curr_idx = write_idx - 1
+    write_idx += a_count - 1
+    final_size = write_idx + 1
+
+    while curr_idx >= 0:
+        if s[curr_idx] == 'a':
+            s[write_idx -1: write_idx + 1] == 'dd'
+            write_idx -=2
+        else:
+            s[write_idx] = s[curr_idx]
+            write_idx -= 1
+        curr_idx -= 1
+
+    return final_size
 
 my_list = ['a', 'a', 'b', 'c']
-replace_and_remove(1, my_list)
+print(replace_and_remove(1, my_list))
