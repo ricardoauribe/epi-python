d2897bdd302ee1d1ef3fa6d6e246c1d9b2a11025
Ricardo Uribe <ricardoauribe@hotmail.com>
2021-03-28 21:05:02 -0600

    search method

--- lists/0_lists.py
+++ lists/0_lists.py
@@ -5,3 +5,10 @@ class ListNode:
 		self.data = data
 		self.next = next
 
+# Search for a key
+
+def search_list(L:ListNode, key: int) ->ListNode:
+	while L and L.data != key:
+		L = L.next
+	# If key was nt present in the list, L will have become null
+	return L
