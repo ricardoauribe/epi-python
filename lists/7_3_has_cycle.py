4332e81b2d1146cb5d4ba1de5119c119e327af5f
Ricardo <ricardoauribe@hotmail.com>
2021-04-16 22:24:40 -0500

    working version

--- lists/7_3_has_cycle.py
+++ lists/7_3_has_cycle.py
@@ -3,3 +3,43 @@
 #Complexity: O(f) where f is the final node to search, wort case could be the last so O(n), since no additional memory
 
 from ListNode import ListNode
+
+def has_cycle(head:ListNode) -> [ListNode]:
+  def cycle_len(end):
+    start = end
+    step = 0
+    while True:
+      step += 1
+      start = start.next
+      if start is end:
+        return step
+
+  fast = head
+  slow = head
+
+  while fast and fast.next:
+    slow = slow.next
+    fast = fast.next.next
+
+    if slow is fast:
+      # Whe achieve the start of the cycle
+      cycle_len_advanced_iter = head
+      for _ in range(cycle_len(slow)):
+        cycle_len_advanced_iter =cycle_len_advanced_iter.next
+
+      it = head
+      # Both iterators advance in tandem
+      while it is not cycle_len_advanced_iter:
+        it = it.next
+        cycle_len_advanced_iter = cycle_len_advanced_iter.next
+      return it # iter is the start of the cycle
+  return None # No cycle
+
+
+com = ListNode(66)
+#com.print_list()
+a = ListNode(11, ListNode(3, ListNode(5, ListNode(7, ListNode(2, com)))))
+com.next = a
+ans = has_cycle(a)
+print(ans.data)
+
